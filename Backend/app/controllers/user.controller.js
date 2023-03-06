1445903ff30954e09877b2bda724a9a3ff3b8498
binura-owin <binura.owin1@gmail.com>
2023-03-06 13:09:11 +0530

    login integration completed

--- Backend/app/controllers/user.controller.js
+++ Backend/app/controllers/user.controller.js
@@ -19,15 +19,24 @@ exports.createUser = asyncHandler(async (req, res) => {
       return makeResponse(res, 200, true, user, "user recorded successfully");
     })
     .catch((err) => {
-      apiErrorLogger.error({
-        type: "internal-error",
-        message: err.message ? err.message : err,
-        success: false,
-      });
+      console.log("internal-error", err.message ? err.message : err)
       next(err);
     });
 });
 
 exports.signUp = asyncHandler(async (req, res) => {
-
+  const query = {
+    email: req.body.email
+  }
+  User.findOne(query).lean({ defaults: true })
+    .then((user) => {
+      if(user.password === req.body.password){
+        return makeResponse(res, 200, user ? true : false, user, user ? "Specific User" : "User not found");
+      }
+      return makeResponse(res, 402, false, null,  "User not found");
+    })
+    .catch((err) => {
+      console.log("internal-error", err.message ? err.message : err)
+      next(err);
+    });
 });
--- FrontEnd/login-app/src/App.js
+++ FrontEnd/login-app/src/App.js
@@ -4,12 +4,13 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom'
+import Login from './screens/Login/Login';
 import Register from './screens/Login/Register';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" >
-      {/* <Route index element={<Login />} /> */}
+      <Route index element={<Login />} />
       <Route path="signup" element={<Register />} />
     </Route>
   )
